7a77f285778db407dbb7e8313d045f30ce40d47d
Wavefarer42 <wavefarer42@gmail.com>
2024-11-06 11:50:43 +0200

    Add openmp however the headers are still not found.

--- tests/test_libraries.cpp
+++ tests/test_libraries.cpp
@@ -1,8 +1,10 @@
 #include "gtest/gtest.h"
-#include "persistence.h"
+#include "bootstrap.h"
 
-TEST(OpenMeshTests, TestIfTriangleMesh) {
-    persistence::MeshDao sut;
+
+TEST(LibraryTests, QuadMeshLoading) {
+    const persistence::MeshDao sut = bootstrap::Container().mesh_dao();
+
     const auto mesh = sut.load_mesh_from_file("../tests/resources/fandisk.obj");
 
     for (auto it_f = mesh.faces_begin(); it_f != mesh.faces_end(); ++it_f) {
